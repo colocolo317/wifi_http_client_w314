fed9c8823479ee956dabd4ee646f363b410bf6d0
colocolo317 <colocolo317@gmail.com>
2024-05-09 14:35:42 +0800

    debug log function improve.

--- mux_debug.c
+++ mux_debug.c
@@ -15,3 +15,38 @@ osMutexId_t mux_debug_init(void)
   return debug_prints_mutex;
 }
 
+#if defined(AMPAK_OS_DEBUG_LOG)
+inline void MUX_LOG(char* format, ...)
+{
+  if(osMutexAcquire(debug_prints_mutex, 200) == osOK)
+  {
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+    osMutexRelease(debug_prints_mutex);
+  }
+  else
+  {
+    printf("%s", __func__ );
+  }
+
+}
+#else
+inline void MUX_LOG(char* format, ...)
+{}
+#endif
+
+#if 0
+inline void http_debug_log(char* format, ...)
+{
+  va_list args;
+  va_start(args, format);
+  vprintf(format, args);
+  va_end(args);
+}
+#else
+inline void http_debug_log(char* format, ...)
+{}
+#endif
+
--- mux_debug.h
+++ mux_debug.h
@@ -10,25 +10,12 @@
 
 #include "cmsis_os2.h"
 #include "stdio.h"
-
-#if defined(AMPAK_OS_DEBUG_LOG)
-#define MUX_LOG(...)                                \
-  {                                                   \
-    if(osMutexAcquire(debug_prints_mutex, 1000) == osOK) \
-    {                                                   \
-        printf(__VA_ARGS__);                              \
-        osMutexRelease(debug_prints_mutex);               \
-    }   \
-    else \
-    { \
-        printf("%s",__func__);\
-    } \
-  }
-#else
-#define MUX_LOG(...)
-#endif
+#include <stdarg.h>
 
 extern osMutexId_t debug_prints_mutex;
 osMutexId_t mux_debug_init(void);
+
+void MUX_LOG(char* format, ...);
+void http_debug_log(char* format, ...);
 
 #endif /* MUX_DEBUG_H_ */
